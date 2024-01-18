82a3e28eb83ff424cdaf6bd69f4659e60e06a12e
MohamadHossein Haghdadi <mmdhossein.haghdadi@gmail.com>
2024-01-18 23:52:38 +0330

    modified run.py load_data

--- run.py
+++ run.py
@@ -1,25 +1,33 @@
 from template import NaiveBayesClassifier
+import time
 
-
-def preprocess(tweet_string):
+def preprocess(tweet_string : str):
     # clean the data and tokenize it
+
     features = []
     # Your Code
     return features
 
-def load_data(data_path):
+def load_data(data_path : str):
     # load the csv file and return the data
     data = []
-    # your code
-    return data
+    with open(data_path, "r") as file:
+        data = map(lambda string: string.split()[2:], file.readlines()[1:])
+    return list(map(lambda _tuple : (preprocess(_tuple[0]), _tuple[1]), data))
 
 
 # train your model and report the duration time
-train_data_path = None 
+start_time = time.time()
+
+train_data_path = input("address for train data:\n") 
 classes = ['positive', 'negative', 'neutral']
 nb_classifier = NaiveBayesClassifier(classes)
 nb_classifier.train(load_data(train_data_path))
 
+end_time = time.time()
+
 test_string = "I love playing football"
+
+print(f"total training time {end_time - start_time}")
 
 print(nb_classifier.classify(preprocess(test_string)))
