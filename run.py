7c45dcbd314206896324fa976a8e36168b21957c
MohamadHossein Haghdadi <mmdhossein.haghdadi@gmail.com>
2024-01-19 00:51:41 +0330

    modified preprocess added tokenizer

--- run.py
+++ run.py
@@ -1,18 +1,27 @@
 from template import NaiveBayesClassifier
 import time
+import nltk
+import string
+
+# config for tokenizer
+nltk.download('punkt')
+nltk.download('stopwords')
+stop_words = set(nltk.corpus.stopwords.words("english")).union(set(string.punctuation))
+tokenizer = nltk.TweetTokenizer()
 
 def preprocess(tweet_string : str):
     # clean the data and tokenize it
 
-    features = []
-    # Your Code
+    words = tokenizer.tokenize(tweet_string)
+    features = [word for word in words if word.lower() not in stop_words]
+
     return features
 
 def load_data(data_path : str):
     # load the csv file and return the data
     data = []
     with open(data_path, "r") as file:
-        data = map(lambda string: string.split()[2:], file.readlines()[1:])
+        data = map(lambda _string: _string.split(',')[2:], file.readlines()[1:])
     return list(map(lambda _tuple : (preprocess(_tuple[0]), _tuple[1]), data))
 
 
