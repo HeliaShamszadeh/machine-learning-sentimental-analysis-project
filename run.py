1680f0d62ee01527df7f2b933fd3e18a9aefc953
MohamadHossein Haghdadi <mmdhossein.haghdadi@gmail.com>
2024-01-19 01:29:53 +0330

    fixed csv reader, added doc typing

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from template import NaiveBayesClassifier
 import time
 import nltk
 import string
+import csv
 
 # config for tokenizer
 nltk.download('punkt')
@@ -20,15 +21,16 @@ def preprocess(tweet_string : str):
 def load_data(data_path : str):
     # load the csv file and return the data
     data = []
-    with open(data_path, "r") as file:
-        data = map(lambda _string: _string.split(',')[2:], file.readlines()[1:])
-    return list(map(lambda _tuple : (preprocess(_tuple[0]), _tuple[1]), data))
+    with open(data_path, "r", newline="") as csv_file:
+        data = map(lambda _line: _line[2:], list(csv.reader(csv_file))[1:])
+    return list(map(lambda _tuple : (preprocess(_tuple[0]), _tuple[2]), data))
 
 
 # train your model and report the duration time
 start_time = time.time()
 
-train_data_path = input("address for train data:\n") 
+# train_data_path = input("address for train data:\n") 
+train_data_path = "train_data.csv"
 classes = ['positive', 'negative', 'neutral']
 nb_classifier = NaiveBayesClassifier(classes)
 nb_classifier.train(load_data(train_data_path))
--- template.py
+++ template.py
@@ -5,18 +5,18 @@
 
 class NaiveBayesClassifier:
 
-    def __init__(self, classes):
+    def __init__(self, classes : list[str]) -> None:
         # initialization: 
         # inputs: classes(list) --> list of label names
         # class_word_counts --> frequency dictionary for each class
         # class_counts --> number of instances of each class
         # vocab --> all unique words  
-        self.classes = None
+        self.classes = classes
         self.class_word_counts = None
-        self.class_counts = None
+        self.class_counts = len(classes)
         self.vocab = None
 
-    def train(self, data):
+    def train(self, data : list[tuple[list, str]]) -> None:
         # training process:
         # inputs: data(list) --> each item of list is a tuple 
         # the first index of the tuple is a list of words and the second index is the label(positive, negative, or neutral)
@@ -25,21 +25,21 @@ class NaiveBayesClassifier:
             pass
             # Your Code
 
-    def calculate_prior(self):
+    def calculate_prior(self) -> float:
         # calculate log prior
         # you can add some attributes to this method
   
         # Your Code
         return None 
 
-    def calculate_likelihood(self, word, label):
+    def calculate_likelihood(self, word : str, label : str) -> float:
         # calculate likelihhood: P(word | label)
         # return the corresponding value
 
         # Your Code
         return None
 
-    def classify(self, features):
+    def classify(self, features : list[str]) -> str:
         # predict the class
         # inputs: features(list) --> words of a tweet 
         best_class = None 
